2b60d0ca5e3c909c1435dafad68dc6f3422f67a5
Mikhail Netkachev <mikhailnetkachev@MacBook-Pro-Mikhail.local>
2020-11-20 00:27:30 +0200

    Fixed issue: edited utils export.

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+import calculateDirection from './calculateDirection';
+import calculateHeight from './calculateHeight';
+import calculatePercent from './calculatePercent';
+
+export {
+  calculateDirection,
+  calculateHeight,
+  calculatePercent,
+};
